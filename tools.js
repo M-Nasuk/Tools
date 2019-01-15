ad55813c6e102d88f7883d8f63e21def21946046
M-Nasuk <m.kusanagi@gmx.fr>
2019-01-15 16:04:56 +0100

    !

--- tools.js
+++ tools.js
@@ -94,3 +94,12 @@ function displayDetails(event)
 
 $( "body" ).click(function( event ) {
   console.log(event.target.id )});
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++ ) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
