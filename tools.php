8c9691c889034e0e0fcd5514651d7476a39617f7
etudiant2 <etudiant2@etudiant-82.home>
2019-07-22 13:54:18 +0200

    add show error php

--- tools.php
+++ tools.php
@@ -73,6 +73,7 @@ function __autoload($classe)
 
   include $path;
 }
-
-
-?>
+/* Show errors in browser */
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
